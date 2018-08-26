4cac0cf57bdd9c58364797db481a42666d5b82db
Mario Domenech Goulart <mario@parenteses.org>
2018-08-26 10:51:14 +0200

    Fix tests (support both CHICKEN 4 and 5)

--- tests/run.scm
+++ tests/run.scm
@@ -1,4 +1,11 @@
-(use introspect)
+(cond-expand
+  (chicken-4
+   (use introspect)
+   (assert (memq 'alist-ref (introspect 'data-structures))))
+  (chicken-5
+   (import introspect)
+   (assert (memq 'string-append (introspect 'scheme))))
+  (else
+   (error "Unsupported CHICKEN version.")))
 
 (assert (equal? '(introspect) (introspect 'introspect)))
-(assert (memq 'alist-ref (introspect 'data-structures)))
